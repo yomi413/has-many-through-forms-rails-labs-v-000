71f3930d0448eb469f05c36840145345db14959e
Yomaira Escano <yjescano@gmail.com>
2018-04-09 06:10:48 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
 
-  # def unique_username
-  #   # self.select(:username).distinct
-  #   self.all.uniq
-  # end
+  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :posts
 end
